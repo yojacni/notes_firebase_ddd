df23d59a58baf9caf4b170d481fe6ec662afd248
Yojacni <57448005+yojacni@users.noreply.github.com>
2022-06-20 18:41:35 -0500

    added AuthFacadeInterface

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:notes_firebase_ddd/domain/auth/auth_failure.dart';
+import 'package:notes_firebase_ddd/domain/auth/value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
